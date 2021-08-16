7714d841af5fc58a413afdb73989a92269b09555
Likith Sai <likithsai13@gmail.com>
2021-08-16 13:57:27 +0530

    Added Dark theme support for InputBar

--- src/components/InputBar.js
+++ src/components/InputBar.js
@@ -1,91 +1,184 @@
 import React from "react";
-import {
-  StyleSheet,
-  View,
-  TextInput,
-  TouchableOpacity,
-  Dimensions,
-} from "react-native";
+import { View, TextInput, TouchableOpacity, Appearance } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
+import { lightTheme, darkTheme } from "../themes/Themes";
 
-const InputBar = (props) => {
-  return (
-    <View style={styles.inputContainer}>
-      {/* <TextInput
-        style={styles.input}
-        onChangeText={(todoInput) => props.textChange(todoInput)}
-        value={props.todoInput}
-        placeholder="Search Items"
-        onSubmitEditing={props.addNewTodo}
-      /> */}
-      <TextInput
-        style={styles.input}
-        onChangeText={(todoInput) => props.textChange(todoInput)}
-        value={props.searchInput}
-        placeholder="Search Items"
-      />
-      <View style={{ flexDirection: "row" }}>
-        <TouchableOpacity onPress={props.addListHandler}>
-          <Ionicons
-            name="add"
-            size={30}
-            color="#555"
-            style={{ marginLeft: 10, marginRight: 5 }}
-          />
-        </TouchableOpacity>
+export default class InputBar extends React.Component {
+  constructor(props) {
+    super(props);
 
-        <TouchableOpacity onPress={props.settingsHandler}>
-          <Ionicons
-            name="md-options-outline"
-            size={30}
-            color="#555"
-            style={{ marginLeft: 5, marginRight: 10 }}
-          />
-        </TouchableOpacity>
+    this.state = {
+      colorScheme: "light",
+    };
+  }
+
+  async componentWillMount() {
+    //  Set theme color for the application
+    this.setState({ colorScheme: Appearance.getColorScheme() });
+  }
+
+  render() {
+    return (
+      <View
+        style={
+          this.state.colorScheme === "light"
+            ? lightTheme.InputBarContainer
+            : darkTheme.InputBarContainer
+        }
+      >
+        <TextInput
+          style={
+            this.state.colorScheme === "light"
+              ? lightTheme.InputBarInput
+              : darkTheme.InputBarInput
+          }
+          onChangeText={(todoInput) => this.props.textChange(todoInput)}
+          value={this.props.searchInput}
+          placeholder="Search Items"
+        />
+        <View style={{ flexDirection: "row" }}>
+          <TouchableOpacity onPress={this.props.addListHandler}>
+            <Ionicons
+              name="add"
+              size={30}
+              color="#555"
+              style={{ marginLeft: 10, marginRight: 5 }}
+            />
+          </TouchableOpacity>
+
+          <TouchableOpacity onPress={this.props.settingsHandler}>
+            <Ionicons
+              name="md-options-outline"
+              size={30}
+              color="#555"
+              style={{ marginLeft: 5, marginRight: 10 }}
+            />
+          </TouchableOpacity>
+        </View>
       </View>
-    </View>
-  );
-};
+    );
+  }
+}
 
-const styles = StyleSheet.create({
-  inputContainer: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    shadowOffset: { width: 0, height: 1 },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 3,
-    marginHorizontal: 20,
-    borderRadius: 10,
-    marginVertical: 10,
-    backgroundColor: "#fff",
-  },
-  input: {
-    width: Dimensions.get("window").width,
-    backgroundColor: "#fff",
-    flex: 1,
-    fontSize: 18,
-    fontWeight: "bold",
-    borderRadius: 10,
-    padding: 15,
-    color: "#555",
-  },
-  addButton: {
-    width: 100,
-    backgroundColor: "#FFCE00",
-    alignItems: "center",
-    justifyContent: "center",
-    flexDirection: "row",
-    alignItems: "center",
-    borderTopRightRadius: 5,
-    borderBottomRightRadius: 5,
-  },
-  addButtonText: {
-    color: "#171717",
-    fontSize: 18,
-    fontWeight: "700",
-  },
-});
+// const styles = StyleSheet.create({
+//   inputContainer: {
+//     flexDirection: "row",
+//     justifyContent: "space-between",
+//     alignItems: "center",
+//     shadowOffset: { width: 0, height: 1 },
+//     shadowOpacity: 0.25,
+//     shadowRadius: 3.84,
+//     elevation: 3,
+//     marginHorizontal: 20,
+//     borderRadius: 10,
+//     marginVertical: 10,
+//     backgroundColor: "#fff",
+//   },
+//   input: {
+//     width: Dimensions.get("window").width,
+//     backgroundColor: "#fff",
+//     flex: 1,
+//     fontSize: 18,
+//     fontWeight: "bold",
+//     borderRadius: 10,
+//     padding: 15,
+//     color: "#555",
+//   },
+//   addButton: {
+//     width: 100,
+//     backgroundColor: "#FFCE00",
+//     alignItems: "center",
+//     justifyContent: "center",
+//     flexDirection: "row",
+//     alignItems: "center",
+//     borderTopRightRadius: 5,
+//     borderBottomRightRadius: 5,
+//   },
+//   addButtonText: {
+//     color: "#171717",
+//     fontSize: 18,
+//     fontWeight: "700",
+//   },
+// });
 
-export default InputBar;
+// const InputBar = (props) => {
+//   return (
+//     <View style={styles.inputContainer}>
+//       {/* <TextInput
+//         style={styles.input}
+//         onChangeText={(todoInput) => props.textChange(todoInput)}
+//         value={props.todoInput}
+//         placeholder="Search Items"
+//         onSubmitEditing={props.addNewTodo}
+//       /> */}
+//       <TextInput
+//         style={styles.input}
+//         onChangeText={(todoInput) => props.textChange(todoInput)}
+//         value={props.searchInput}
+//         placeholder="Search Items"
+//       />
+//       <View style={{ flexDirection: "row" }}>
+//         <TouchableOpacity onPress={props.addListHandler}>
+//           <Ionicons
+//             name="add"
+//             size={30}
+//             color="#555"
+//             style={{ marginLeft: 10, marginRight: 5 }}
+//           />
+//         </TouchableOpacity>
+
+//         <TouchableOpacity onPress={props.settingsHandler}>
+//           <Ionicons
+//             name="md-options-outline"
+//             size={30}
+//             color="#555"
+//             style={{ marginLeft: 5, marginRight: 10 }}
+//           />
+//         </TouchableOpacity>
+//       </View>
+//     </View>
+//   );
+// };
+
+// const styles = StyleSheet.create({
+//   inputContainer: {
+//     flexDirection: "row",
+//     justifyContent: "space-between",
+//     alignItems: "center",
+//     shadowOffset: { width: 0, height: 1 },
+//     shadowOpacity: 0.25,
+//     shadowRadius: 3.84,
+//     elevation: 3,
+//     marginHorizontal: 20,
+//     borderRadius: 10,
+//     marginVertical: 10,
+//     backgroundColor: "#fff",
+//   },
+//   input: {
+//     width: Dimensions.get("window").width,
+//     backgroundColor: "#fff",
+//     flex: 1,
+//     fontSize: 18,
+//     fontWeight: "bold",
+//     borderRadius: 10,
+//     padding: 15,
+//     color: "#555",
+//   },
+//   addButton: {
+//     width: 100,
+//     backgroundColor: "#FFCE00",
+//     alignItems: "center",
+//     justifyContent: "center",
+//     flexDirection: "row",
+//     alignItems: "center",
+//     borderTopRightRadius: 5,
+//     borderBottomRightRadius: 5,
+//   },
+//   addButtonText: {
+//     color: "#171717",
+//     fontSize: 18,
+//     fontWeight: "700",
+//   },
+// });
+
+// export default InputBar;
--- src/themes/Themes.js
+++ src/themes/Themes.js
@@ -2,7 +2,6 @@ import { StyleSheet } from "react-native";
 
 //  Light Theme
 const lightTheme = StyleSheet.create({
-
   //  Styles for Home
   container: {
     flex: 1,
@@ -27,26 +26,98 @@ const lightTheme = StyleSheet.create({
     alignItems: "center",
     backgroundColor: "#f0f0f0",
   },
-  headerStyleIcon : {
-    marginRight: 10, 
-    color: "#666"
+  headerStyleIcon: {
+    marginRight: 10,
+    color: "#666",
   },
-  headerStyleCategoryText : {
-    color: "#000"
+  headerStyleCategoryText: {
+    color: "#000",
   },
   footerTextStyle: {
     color: "#666",
-  }
+  },
 
+  // InputBar Component
+  InputBarContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 3,
+    marginHorizontal: 20,
+    borderRadius: 10,
+    marginVertical: 10,
+    backgroundColor: "#fff",
+  },
+  InputBarInput: {
+    width: "100%",
+    backgroundColor: "#fff",
+    flex: 1,
+    fontSize: 18,
+    fontWeight: "bold",
+    borderRadius: 10,
+    padding: 15,
+    color: "#555",
+  },
+
+  //  TODOItems
+  TODOItemsContainer: {
+    flexDirection: "row",
+    alignItems: "flex-start",
+    width: "100%",
+  },
+  TODOItems: {
+    width: '100%',
+    borderBottomColor: "#DDD",
+    borderBottomWidth: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingHorizontal: 30,
+    paddingVertical: 20,
+  },
+  TODOItemsTitleSelected: {
+    fontWeight: "bold",
+    color: "#4B0082",
+    fontSize: 18,
+  },
+  TODOItemsTitleUnselected: {
+    fontWeight: "bold",
+    color: "rgba(75, 0, 130, 0.6)",
+    fontSize: 18,
+  },
+  TODOItemsBoxShadowWithSelected: {
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 3,
+    borderRadius: 0,
+    backgroundColor: "#fff",
+  },
+  TODOItemsDateText: {
+    fontSize: 15,
+    color: "#aaa",
+    marginVertical: 10,
+  },
+  TODOItemsCategoryText: {
+    fontSize: 15,
+    color: "#aaa",
+  },
+  TODOItemsCategoryText: {
+    marginVertical: 10,
+    color: "#666",
+    flexDirection: "row",
+    alignItems: "center",
+  },
 });
 
 //  Dark Theme
 const darkTheme = StyleSheet.create({
-
   //  Styles for Home
   container: {
     flex: 1,
-    backgroundColor: "#232323",
+    backgroundColor: "#261C2C",
   },
   emptyListStyle: {
     flex: 1,
@@ -65,19 +136,43 @@ const darkTheme = StyleSheet.create({
     paddingHorizontal: 10,
     marginBottom: 10,
     alignItems: "center",
-    backgroundColor: "#232323",
+    backgroundColor: "#261C2C",
   },
-  headerStyleIcon : {
-    marginRight: 10, 
-    color: "#999"
+  headerStyleIcon: {
+    marginRight: 10,
+    color: "#999",
   },
-  headerStyleCategoryText : {
-    color: "#999"
+  headerStyleCategoryText: {
+    color: "#999",
   },
   footerTextStyle: {
     color: "#999",
-  }
-  
+  },
+
+  // InputBar Component
+  InputBarContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 3,
+    marginHorizontal: 20,
+    borderRadius: 10,
+    marginVertical: 10,
+    backgroundColor: "#3E2C41",
+  },
+  InputBarInput: {
+    width: "100%",
+    backgroundColor: "#3E2C41",
+    flex: 1,
+    fontSize: 18,
+    fontWeight: "bold",
+    borderRadius: 10,
+    padding: 15,
+    color: "#ccc",
+  },
 });
 
 export { lightTheme, darkTheme };
